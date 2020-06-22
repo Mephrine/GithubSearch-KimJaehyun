347fdd874877936a904c80f5b8a252aa72e81997
Mephrine <dthwrld@gmail.com>
2020-06-22 13:19:11 +0900

    create repository

---
+++ GithubSearch/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  GithubSearch
+//
+//  Created by Mephrine on 2020/06/22.
+//  Copyright © 2020 Mephrine. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Light모드로만 진행
+        if #available(iOS 13.0, *) {
+            self.window?.overrideUserInterfaceStyle = .light
+        }
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+}
+
---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Mephrine on 2020/06/22.
+//  Copyright © 2020 Mephrine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
